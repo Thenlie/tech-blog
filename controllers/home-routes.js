dbeecfe8a84c9b1c1a837bc791ba84777fc226f0
Leithen <leithen113@gmail.com>
2021-12-11 18:10:11 -0700

    Fixed post sort order

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,7 +7,8 @@ router.get('/', async (req, res) => {
             attributes: ['id', 'title', 'content', 'user_id', 'createdAt'],
             include: [
                 { model: User, attributes: ['username'] }, 
-                { model: Comment, include: { model: User } }]
+                { model: Comment, include: { model: User } }],
+            order: [['id', 'DESC']]
         });
         const posts = response.map(post => post.get({ plain: true }));
         res.render('homepage', { posts, loggedIn: req.session.loggedIn, home: true });
